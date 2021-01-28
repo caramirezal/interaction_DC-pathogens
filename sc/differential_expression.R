0534332bdcc431e5809f44b537067cffcdb100c2
caramirezal <caramirezal@ciencias.unam.mx>
2021-01-28 14:33:42 +0100

    All DEG comparissons

--- sc/differential_expression.R
+++ sc/differential_expression.R
@@ -14,7 +14,7 @@ set.seed(333)
 path2project <- '/Users/carlosramirez/sc/interaction_DC-pathogens/'
 setwd(path2project)
 
-path2seurat <- '/Users/carlosramirez/sc/interaction_DC-pathogens/data/batch_correction/batch_corrected_seu.rds'
+path2seurat <- '/Users/carlosramirez/sc/interaction_DC-pathogens/data/batch_correction/seurat_batch_corrected.rds'
 umap1_th <- 1.5
 
 seurat <- readRDS(path2seurat)
@@ -23,68 +23,75 @@ seurat
 DimPlot(seurat, reduction = 'pca')
 
 ## UMAP showing batch corrected data
+pdf('figures/umap_filter_threshold.pdf')
 DimPlot(seurat) +
         geom_vline(xintercept = umap1_th,
                    size=1.1, 
                    colour='red',
                    linetype='dashed')
-cells <- colnames(seurat)[
-        seurat@reductions$umap@cell.embeddings[,'UMAP_1'] < umap1_th
-]
+dev.off()
+
+
 ## After removing outlier P193 cluster
+cells <- colnames(seurat)[
+  seurat@reductions$umap@cell.embeddings[,'UMAP_1'] < umap1_th
+]
 DimPlot(subset(seurat, cells=cells))
 
 ## Subsetting to cells without P193 cluster
-if ( 0 < length(cells)){
-        seurat.filtered <-  subset(seurat, cells=cells) 
-} else {
-        seurat.filtered <- seurat
-}
-seurat
-seurat.filtered
-rm(seurat)
+#if ( 0 < length(cells)){
+#        seurat.filtered <-  subset(seurat, cells=cells) 
+#} else {
+#        seurat.filtered <- seurat
+#}
+#seurat
+#seurat.filtered
+#rm(seurat)
 
-## Visualization of categories
-DimPlot(seurat.filtered, pt.size = 2) +
-                theme(legend.position = 'top')
-DimPlot(seurat.filtered, group.by = 'disease_state') +
-        theme(legend.position = 'top')
-DimPlot(seurat.filtered, 
-        group.by = 'bacteria_attached') +
-        theme(legend.position = 'top')
-DimPlot(seurat.filtered, 
-        group.by = 'progenitor_type') +
-        theme(legend.position = 'top')
-DimPlot(seurat.filtered, 
-        group.by = 'treatment') +
-        theme(legend.position = 'top')
 
 #####################################################################
 ## Differential Expression Analysis
 
 ## Comparing MDP vs CMP
 
-## Settings
-n_top_deg <- 40
+## Auxiliary function to perform DEA
+dea <- function(seurat,
+                ident,
+                group_by){
+  deg <- FindMarkers(seurat, 
+                     ident.1 = ident, 
+                     group.by = group_by, 
+                     logfc.threshold = 0, 
+                     min.pct = 0, 
+                     min.cells.feature = 1)
+  #deg <- filter(deg, p_val_adj < 0.05) 
+  deg <- arrange(deg, desc(abs(avg_logFC)))
+  
+  return(deg)
+}
 
 ## Comparing MDP vs CDP
-progenitor.deg <- FindMarkers(seurat.filtered, 
-                              ident.1 = 'MDP', 
-                              group.by = 'progenitor_type', 
-                              logfc.threshold = 0, 
-                              min.pct = 0)
-progenitor.deg <- filter(progenitor.deg, p_val_adj < 0.05) 
-progenitor.deg <- arrange(progenitor.deg, desc(abs(avg_logFC)))
+progenitor.deg <- dea(seurat = seurat,
+                      ident =  'MDP',
+                      group_by = 'progenitor_type')
 dim(progenitor.deg)
+head(progenitor.deg)
 
-progenitor.mtx <- seurat.filtered@assays$integrated@scale.data[
+## Settings
+n_top_deg <- 50
+
+progenitor.mtx <- seurat@assays$integrated@scale.data[
         rownames(progenitor.deg)[1:n_top_deg],
 ]
 dim(progenitor.mtx)
-seurat.filtered
 
 ## Heatmap visualization
-col_df <- select(seurat.filtered@meta.data, progenitor_type)
+col_df <- select(seurat@meta.data, 
+                 progenitor_type,
+                 plate_number,
+                 disease_state, 
+                 bacteria_attached,
+                 treatment)
 head(col_df)
 col_anns <- list(progenitor_type=c(CDP='steelblue',
                                    MDP='salmon'))
@@ -95,70 +102,276 @@ pheatmap(scale(progenitor.mtx),
 
 ################################################################
 ## Comparing MDP (1 and 24 hours)
+
+## Rename cells
+## Create a column named infected_vs_bystander
+## MOI 0 -> control
+## MOI != 0 -> bystander
+## bacteria attached (+) -> infected 
+seurat$treatment[is.na(seurat$treatment)] <- 'NA'
+seurat$'infection_status' <- 'NA'
+seurat$infection_status <- sapply(
+  seurat$treatment, function(x)
+    ifelse(x=='MOI 0', 'Control', 'Bystander')
+)
+seurat$infection_status <- sapply(
+  1:ncol(seurat), function(i)
+    ifelse(seurat$bacteria_attached[i]=='+', 
+           'Infected', seurat$infection_status[i])
+)
+table(seurat$treatment,
+      seurat$infection_status)
+
+################################################################
+##                                                            ##
+##            Differential Expression Analysis                ##
+##                                                            ##
+################################################################
+
+## Directory to store results
+dir.create('data/dea/')
+
+###############################################################
+## MDPs
+
+## 1hr
 ##  * A vs B pos
+## Infected vs control
+degs.infVsCtrl.1h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 1h post-infection' &
+                    progenitor_type == 'MDP' & 
+                    infection_status %in% c('Infected', 'Bystander')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.1h, n = 10)
+dim(degs.infVsCtrl.1h)
+write.table(degs.infVsCtrl.1h,
+      file = gzfile('data/dea/degs.mdp.infVsCtrl.1h.tsv.gz'),
+      sep = '\t', 
+      quote = FALSE, 
+      row.names = TRUE
+)
+
 ##  * A vs B neg
+## Bystander vs control
+degs.bysVsCtrl.1h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 1h post-infection' &
+                    progenitor_type == 'MDP' & 
+                    infection_status %in% c('Control', 'Bystander')),
+  ident = 'Bystander',
+  group_by = 'infection_status'
+)
+head(degs.bysVsCtrl.1h, n = 10)
+dim(degs.bysVsCtrl.1h)
+write.table(degs.bysVsCtrl.1h,
+            file = gzfile('data/dea/degs.mdp.bysVsCtrl.1h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
 ##  * B pos vs B neg
-
-seurat.filtered$treatment[is.na(seurat.filtered$treatment)] <- 'NA'
-dittoBarPlot(seurat.filtered, 
-             "bacteria_attached", 
-             group.by = "treatment", 
-             scale = 'count')
-hist()
-
-## Re-label infection cell status 
-seurat.filtered$'treatment_bacteria' <- paste0(
-        seurat.filtered$treatment, '_',
-        seurat.filtered$bacteria_attached
+## Infected vs Bystander
+degs.infVsBys.1h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 1h post-infection' &
+                    progenitor_type == 'MDP' & 
+                    infection_status %in% c('Infected', 'Bystander')),
+  ident = 'Infected',
+  group_by = 'infection_status'
 )
-table(seurat.filtered$treatment_bacteria)
-table(seurat.filtered$bacteria,
-      seurat.filtered$bacteria_attached)
-seurat.filtered$treatment_bacteria <-
-        gsub(' ', '_', seurat.filtered$treatment_bacteria)
-seurat.filtered$treatment_bacteria %>% table()
-seurat.filtered$'infected_vs_bystander' <- plyr::mapvalues(
-        seurat.filtered$treatment_bacteria,
-        from = unique(seurat.filtered$treatment_bacteria),
-        to = c('Non-treated', 'Infected', '')
+head(degs.infVsCtrl.1h, n = 10)
+dim(degs.infVsCtrl.1h)
+write.table(degs.infVsCtrl.1h,
+            file = gzfile('data/dea/degs.mdp.infVsBys.1h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
 )
-hist(seurat.filtered$Y582_15_A_RFP, breaks = 1000)
-unique(seurat.filtered$treatment_bacteria)
-
-time <- 'Yesinia 1h post-infection'
-progenitor <- 'MDP'
 
 
+## 24hr
+##  * A vs B pos
+## Infected vs control
+degs.infVsCtrl.24h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 24h post-infection' &
+                    progenitor_type == 'MDP' & 
+                    infection_status %in% c('Infected', 'Control')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.24h, n = 10)
+dim(degs.infVsCtrl.24h)
+write.table(degs.infVsCtrl.24h,
+            file = gzfile('data/dea/degs.mdp.infVsCtrl.24h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+##  * A vs B neg
+## Bystander vs control
+degs.bysVsCtrl.24h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 24h post-infection' &
+                    progenitor_type == 'MDP' & 
+                    infection_status %in% c('Control', 'Bystander')),
+  ident = 'Bystander',
+  group_by = 'infection_status'
+)
+head(degs.bysVsCtrl.24h, n = 10)
+dim(degs.bysVsCtrl.24h)
+write.table(degs.bysVsCtrl.24h,
+            file = gzfile('data/dea/degs.mdp.bysVsCtrl.24h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+##  * B pos vs B neg
+## Infected vs Bystander
+degs.infVsBys.24h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 24h post-infection' &
+                    progenitor_type == 'MDP' & 
+                    infection_status %in% c('Infected', 'Bystander')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.24h, n = 10)
+dim(degs.infVsCtrl.24h)
+write.table(degs.infVsCtrl.24h,
+            file = gzfile('data/dea/degs.mdp.infVsBys.24h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+###############################################################
+###############################################################
+## CDPs
+
+## 1hr
+##  * A vs B pos
+## Infected vs control
+degs.infVsCtrl.1h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 1h post-infection' &
+                    progenitor_type == 'CDP' & 
+                    infection_status %in% c('Infected', 'Bystander')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.1h, n = 10)
+dim(degs.infVsCtrl.1h)
+write.table(degs.infVsCtrl.1h,
+            file = gzfile('data/dea/degs.cdp.infVsCtrl.1h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+##  * A vs B neg
+## Bystander vs control
+degs.bysVsCtrl.1h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 1h post-infection' &
+                    progenitor_type == 'CDP' & 
+                    infection_status %in% c('Control', 'Bystander')),
+  ident = 'Bystander',
+  group_by = 'infection_status'
+)
+head(degs.bysVsCtrl.1h, n = 10)
+dim(degs.bysVsCtrl.1h)
+write.table(degs.bysVsCtrl.1h,
+            file = gzfile('data/dea/degs.cdp.bysVsCtrl.1h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+##  * B pos vs B neg
+## Infected vs Bystander
+degs.infVsBys.1h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 1h post-infection' &
+                    progenitor_type == 'CDP' & 
+                    infection_status %in% c('Infected', 'Bystander')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.1h, n = 10)
+dim(degs.infVsCtrl.1h)
+write.table(degs.infVsCtrl.1h,
+            file = gzfile('data/dea/degs.cdp.infVsBys.1h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
 
 
-## Helper function to perform DEA
-deg <- function(seurat, 
-                reference='',
-                deg_column=''){
-        seurat <- FindMarkers(seurat, 
-                              ident.1 = '+', 
-                              group.by = 'bacteria_attached', 
-                              logfc.threshold = 0, 
-                              min.pct = 0, 
-                              min.cells.feature = 0, 
-                              min.cells.group = 1)
-        seurat <- filter(seurat, p_val < 0.01) 
-        seurat <- arrange(seurat, desc(abs(avg_logFC)))
-        
-        col_df <- select(mdp@meta.data, 
-                         bacteria_attached, 
-                         disease_state) 
-        col_df$bacteria_attached <- ifelse(col_df$bacteria_attached == '+',
-                                           'Ye', 'No_Ye')
-        col_df$disease_state <- ifelse(col_df$disease_state == 'Yesinia 1h post-infection',
-                                       'treatment_1h', 'treatment_24h')
-        col_anns <- list(bacteria_attached=c(Ye='steelblue',
-                                             No_Ye='salmon'),
-                         disease_state=c(treatment_1h='green',
-                                         treatment_24h='orange'))
-        pheatmap(scale(mdp.inf_byst.mtx), 
-                 show_colnames = FALSE, 
-                 annotation_col = col_df,
-                 annotation_colors = col_anns)
-}
+## 24hr
+##  * A vs B pos
+## Infected vs control
+degs.infVsCtrl.24h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 24h post-infection' &
+                    progenitor_type == 'CDP' & 
+                    infection_status %in% c('Infected', 'Control')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.24h, n = 10)
+dim(degs.infVsCtrl.24h)
+write.table(degs.infVsCtrl.24h,
+            file = gzfile('data/dea/degs.cdp.infVsCtrl.24h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
 
+##  * A vs B neg
+## Bystander vs control
+degs.bysVsCtrl.24h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 24h post-infection' &
+                    progenitor_type == 'CDP' & 
+                    infection_status %in% c('Control', 'Bystander')),
+  ident = 'Bystander',
+  group_by = 'infection_status'
+)
+head(degs.bysVsCtrl.24h, n = 10)
+dim(degs.bysVsCtrl.24h)
+write.table(degs.bysVsCtrl.24h,
+            file = gzfile('data/dea/degs.cdp.bysVsCtrl.24h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+##  * B pos vs B neg
+## Infected vs Bystander
+degs.infVsBys.24h <- dea(
+  seurat = subset(seurat, 
+                  disease_state == 'Yesinia 24h post-infection' &
+                    progenitor_type == 'CDP' & 
+                    infection_status %in% c('Infected', 'Bystander')),
+  ident = 'Infected',
+  group_by = 'infection_status'
+)
+head(degs.infVsCtrl.24h, n = 10)
+dim(degs.infVsCtrl.24h)
+write.table(degs.infVsCtrl.24h,
+            file = gzfile('data/dea/degs.cdp.infVsBys.24h.tsv.gz'),
+            sep = '\t', 
+            quote = FALSE, 
+            row.names = TRUE
+)
+
+ch <- read.table('data/dea/degs.cdp.bysVsCtrl.1h.tsv.gz',
+                 header = TRUE)
+head(ch)
